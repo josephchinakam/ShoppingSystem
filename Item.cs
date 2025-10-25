f5991b0e3bf8d913bdbdfc6e1b752d0d6d1cd91b
Josep Simbarashe Chinaka II <106067955+josephchinakam@users.noreply.github.com>
2025-10-25 11:37:05 +0200

    Add Item class for shopping cart system

---
+++ Item.cs
@@ -0,0 +1,14 @@
+namespace ShoppingCartSystem
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public Item(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
